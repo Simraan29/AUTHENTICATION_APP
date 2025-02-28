37fdffabb52e833c5bba0c4c4a5f33df017b6df8
simraan <2203051050551@paruluniversity.ac.in>
2025-02-28 23:03:01 +0530

    db

--- server/index.js
+++ server/index.js
@@ -1,42 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config(); // Load environment variables from .env
 const authRouter = require('./routes/authRoute');
 
 const app = express();
-
 
 // 1) Middlewares
 app.use(cors());
 app.use(express.json());
 
-// 2) Route
-
+// 2) Routes
 app.use('/api/auth', authRouter);
 
-// 3) Moongo DB Connection
+// 3) MongoDB Connection
 mongoose.set('strictQuery', true);
+const mongoURI = process.env.MONGODB_URI;
+if (!mongoURI) {
+  console.error('Error: MONGODB_URI environment variable not set.');
+  process.exit(1);
+}
 
-mongoose.connect('mongodb://localhost:27017/authentication')
-.then(() => console.log('connected to MongoDB!'))
-.catch((error)=>console.error('Failed to connect to MongoDB:',error));
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((error) => console.error('Failed to connect to MongoDB:', error));
 
 // 4) Global error handler
- 
-app.use((err,req,res, next) => {
-    err.statuCode = err.statuCode || 500;
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statuCode).json({
+    res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
     });
-}); 
-
-// 5) server 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`App running on ${PORT}`); 
 });
 
- 
+// 5) Server 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
